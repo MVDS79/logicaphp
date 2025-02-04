fd5cf4d84567068ba9957c9bac1c81c7ad842b51
Marcio Valente da Silva <VAS0036950@aluno.firjansenaisesi.com.br>
2025-02-04 19:18:32 -0300

    funcionalidade de previsao do tempo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Alberto Roberto";
+$idade = 20;
+if ($idade >= 18) {
+    echo $nome . ", Voce e maior de idade ";
+} else {
+    echo $nome . ", Voce e menor de idade ";
+}
+
+$nome = "<br>Zé Bonitinho";
+$idade = 16;
+if ($idade >= 18) {
+    echo $nome . ", Voce e maior de idade ";
+} else {
+    echo $nome . ", Voce e menor de idade ";
+}
